52e563b5c3312e335da8d7bf91c1b7bc8cccae6a
Ahmad Berri <ahmad.berri@basma.com>
2023-07-17 21:45:12 +0300

    Lesson 163

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,14 +4,14 @@ import Category from "../category/category.component";
 import { useEffect } from "react";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import { useDispatch } from "react-redux";
-import { setCategoriesMap } from "../../store/categories/category.action";
+import { setCategories } from "../../store/categories/category.action";
 
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      dispatch(setCategoriesMap(categoryMap));
+      const categoryArray = await getCategoriesAndDocuments();
+      dispatch(setCategories(categoryArray));
     };
 
     void getCategoriesMap();
