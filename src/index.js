75176cf4ac869ba20acb1413ccaa8b87b11a9ec2
Ahmad Berri <ahmad.berri@basma.com>
2023-07-25 19:36:10 +0300

    Lesson 167

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {CartProvider} from "./contexts/cart.context";
-import {store} from "./store/store";
+import { CartProvider } from "./contexts/cart.context";
+import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CartProvider>
-          <App/>
-        </CartProvider>
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
