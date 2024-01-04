9e4bab8954ff6cecb7fcee1ffbf9eaf3920e5760
Ahmad Berri <ahmad.berri@basma.com>
2024-01-04 22:06:47 +0200

    Lesson 180 (BUG)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import { useEffect } from "react";
-import {
-  createUserDocumentFromAuth,
-  onAuthStateChangedListener,
-} from "./utils/firebase/firebase.utils";
-import { setCurrentUser } from "./store/user/user.action";
+import { getCurrentUser } from "./utils/firebase/firebase.utils";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
 import { useDispatch } from "react-redux";
+import { checkUserSession } from "./store/user/user.action";
 
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    return onAuthStateChangedListener(async (user) => {
-      if (user) {
-        await createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
+    dispatch(checkUserSession());
   }, []);
   return (
     <Routes>
